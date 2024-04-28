d05e031f2ecbd2d70ee8389bc356f98230d4d0aa
Harykaran Lambotharan <137681699+LANDEX-E@users.noreply.github.com>
2024-04-28 14:20:03 +0200

    Update ViewModel.java

--- src/ViewModel.java
+++ src/ViewModel.java
@@ -1,14 +1,15 @@
+package j_c.package;
+
 import io.jenkins.plugins.echarts.api.PieChartModel;
 import io.jenkins.plugins.echarts.api.PieData;
 import io.jenkins.plugins.util.JacksonFacade;
 
 public class ViewModel {
     public static String getPieChartModelJson() {
-        PieChartModel model = new PieChartModel("Your Chart Title");
-        model.add(new PieData("Segment 1 name", 10), Palette.RED);
-        model.add(new PieData("Segment 2 name", 15), Palette.GREEN);
-        model.add(new PieData("Segment 3 name", 20), Palette.YELLOW);
-
+        PieChartModel model = new PieChartModel("Chart Title");
+        model.add(new PieData("Segment 1", 10), PieChartModel.Palette.RED);
+        model.add(new PieData("Segment 2", 20), PieChartModel.Palette.GREEN);
+        model.add(new PieData("Segment 3", 30), PieChartModel.Palette.BLUE);
         return new JacksonFacade().toJson(model);
     }
 }
